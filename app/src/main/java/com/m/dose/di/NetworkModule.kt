9b9d2288ea90c0334a7ee89f99736979e63bbdac
Mohamed Ahmed <momo46060@gmail.com>
2023-11-11 20:54:29 +0200

    Setup App structure

---
+++ app/src/main/java/com/m/dose/di/NetworkModule.kt
@@ -0,0 +1,11 @@
+package com.m.dose.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+}
