390e009bc0aaed7d65b32065fd96e600582e63a0
Mohamed Ahmed <momo46060@gmail.com>
2023-11-12 09:04:36 +0200

    Edit Medicen Object

--- domain/src/main/java/com/m/domain/model/Medecin.kt
+++ domain/src/main/java/com/m/domain/model/Medecin.kt
@@ -1,8 +1,10 @@
 package com.m.domain.model
 
 data class Medecin(
+    val image:String,
     val name:String,
     val sitName:String,
     val dose:String,
     val dis:String,
+    val useFor:String,
 )
