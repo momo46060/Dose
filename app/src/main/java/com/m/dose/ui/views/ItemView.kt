9f6582b4e41aab79575364979b1e980f1560cbeb
Mohamed Ahmed <momo46060@gmail.com>
2023-11-16 10:21:06 +0200

    fix text alignment

--- app/src/main/java/com/m/dose/ui/views/ItemView.kt
+++ app/src/main/java/com/m/dose/ui/views/ItemView.kt
@@ -1,31 +1,23 @@
 package com.m.dose.ui.views
 
 import android.content.res.Configuration
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
-import coil.compose.rememberAsyncImagePainter
 import com.m.domain.model.Medecin
-import com.m.dose.ui.theme.Blue
-import com.m.dose.ui.theme.ShdowColor
+import com.m.dose.ui.theme.CardBackground
 import com.m.dose.ui.theme.subTitleStyle
+import com.m.dose.ui.theme.textColor
 import com.m.dose.ui.theme.titleStyle
 import com.m.dose.utils.CARD_HEIGHT
 import com.m.dose.utils.L_PADDING
@@ -33,65 +25,63 @@ import com.m.dose.utils.S_PADDING
 
 @Composable
 fun ItemView(medecin: Medecin) {
-    Card(
+    Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .height(CARD_HEIGHT)
-            .shadow(L_PADDING, shape = RoundedCornerShape(L_PADDING), spotColor = ShdowColor)
-            .padding(S_PADDING),
-        colors = CardDefaults.cardColors(Color.White),
-        border = BorderStroke(2.dp, Blue)
+            .background(CardBackground)
+            .height(CARD_HEIGHT),
     ) {
         ConstraintLayout(
             modifier = Modifier
                 .fillMaxSize()
+                .background(CardBackground)
                 .padding(L_PADDING)
-                .padding(S_PADDING)
         ) {
-            val (image, title, sitName, dis) = createRefs()
-            Image(
-                modifier = Modifier
-                    .constrainAs(image) {
-                        start.linkTo(parent.start)
-                        end.linkTo(title.start)
-                        width = Dimension.value(100.dp)
-                        height = Dimension.fillToConstraints
-                    },
-                contentScale = ContentScale.Crop,
-                painter = rememberAsyncImagePainter(model = medecin.image),
-                contentDescription = null
-            )
+            val ( title, sitName, dis) = createRefs()
             Text(
                 modifier = Modifier.constrainAs(title) {
                     top.linkTo(parent.top)
-                    start.linkTo(image.end,S_PADDING)
-                    width = Dimension.fillToConstraints
+                    start.linkTo(parent.absoluteLeft,S_PADDING)
                     height = Dimension.wrapContent
-                },
+                    width = Dimension.matchParent
+                }
+                ,
                 text = medecin.name,
-                style = titleStyle
+                style = titleStyle,
+                color = textColor
             )
             Text(
-                modifier = Modifier.constrainAs(sitName) {
-                    top.linkTo(title.bottom, S_PADDING)
-                    start.linkTo(image.end,S_PADDING)
-                    height = Dimension.wrapContent
-                }.fillMaxWidth(.7f),
+                modifier = Modifier
+                    .constrainAs(sitName) {
+                        top.linkTo(title.bottom, S_PADDING)
+                        start.linkTo(parent.absoluteLeft, S_PADDING)
+                        height = Dimension.wrapContent
+                        width = Dimension.matchParent
+
+                    }
+                    .fillMaxWidth(.95f),
                 text = medecin.sitName,
                 style = subTitleStyle,
                 overflow = TextOverflow.Ellipsis,
-                maxLines = 2,
+                maxLines = 1,
+                color = textColor,
+
             )
             Text(
-                modifier = Modifier.fillMaxWidth(.7f).constrainAs(dis) {
-                    top.linkTo(sitName.bottom, S_PADDING)
-                    start.linkTo(image.end,S_PADDING)
-                    height = Dimension.wrapContent
-                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(dis) {
+                        top.linkTo(sitName.bottom, S_PADDING)
+                        start.linkTo(parent.absoluteLeft, S_PADDING)
+                        height = Dimension.wrapContent
+                        width = Dimension.matchParent
+                    },
                 text = medecin.dis,
                 style = subTitleStyle,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1,
+                color = textColor
+
             )
 
         }
